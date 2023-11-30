2d9b4b06aca3f4bbabfab953c3a712ec2e32d06c
Bandiu <Bandiubandi@gmail.com>
2023-11-30 17:52:20 +0300

    implement paginate

--- src/main/java/org/bandiu/homeWorkSpringBoot/controller/GuestBoockController.java
+++ src/main/java/org/bandiu/homeWorkSpringBoot/controller/GuestBoockController.java
@@ -8,22 +8,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.data.domain.Page;
 
 
 @Controller
 @RequestMapping("/guestbook")
 public class GuestBoockController {
+    private final int pageSize = 5;
 
     @Autowired
     CommentService commentService;
 
     @GetMapping
-    public String showGuestBook(Model model){
-        List<Comment> comments  = commentService.getAllComments();
-        model.addAttribute("comments",comments);
+    public String showGuestBook(@RequestParam(name = "page",defaultValue = "0") int page, Model model){
+        Page<Comment> commentPage = commentService.getPaginatedComments(page,pageSize);
+        model.addAttribute("currentPage",page);
+        model.addAttribute("totalPages",commentPage.getTotalElements());
+        model.addAttribute("comments",commentPage);
         return "guestbook";//toDo need make template FreeMarker
     }
 
