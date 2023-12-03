cc56e06189545cec938769871a47a0963a6511e2
Bandiu <Bandiubandi@gmail.com>
2023-12-03 23:41:47 +0300

    rename to content

--- src/main/java/org/bandiu/homeWorkSpringBoot/model/Comment.java
+++ src/main/java/org/bandiu/homeWorkSpringBoot/model/Comment.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 
 public class Comment {
     private String name;
-    private String comment;
+    private String content;
     private int rating;
     private LocalDateTime createdDate;
 
@@ -14,8 +14,8 @@ public class Comment {
         this.name = name;
     }
 
-    public void setComment(String comment) {
-        this.comment = comment;
+    public void setContent(String content) {
+        this.content = content;
     }
 
     public void setRating(int rating) {
@@ -26,8 +26,8 @@ public class Comment {
         return name;
     }
 
-    public String getComment() {
-        return comment;
+    public String getContent() {
+        return content;
     }
 
     public int getRating() {
